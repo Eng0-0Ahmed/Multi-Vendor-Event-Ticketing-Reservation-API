5e31abd26d7a0c119d812742cb294512a433f1ee
engh <engahmedaymen93@gmail.com>
2026-08-02 01:47:56 +0300

    updating .env.example, changing to Postgres, updating CI and adding docker

--- Ticket_Vendor_Event/settings.py
+++ Ticket_Vendor_Event/settings.py
@@ -175,3 +175,14 @@ STORAGES = {
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "ticketing_db"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
