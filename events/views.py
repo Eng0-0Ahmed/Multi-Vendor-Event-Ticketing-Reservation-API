ed4d80e0422319725aeefae533f68738c0428ceb
engh <engahmedaymen93@gmail.com>
2026-08-04 00:12:50 +0300

    fix(events/views): fixing json bug

--- events/views.py
+++ events/views.py
@@ -56,7 +56,7 @@ class EventCreateView(generics.CreateAPIView):
             "email": self.request.user.email,
             "event_uuid": str(event.uuid),
             "event_title": event.title,
-            "event_vendor": event.vendor,
+            "event_vendor": str(event.vendor),
         }
         redis_client.rpush("notifications", json.dumps(payload))
 
